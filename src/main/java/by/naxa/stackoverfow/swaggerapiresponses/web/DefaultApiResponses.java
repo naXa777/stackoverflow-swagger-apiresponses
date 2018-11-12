af194622f6f28b616753bb19e73663b61b5f5bb8
naXa! <naxa@nextmail.ru>
2018-11-12 23:16:48 +0300

    specify annotation target

--- src/main/java/by/naxa/stackoverfow/swaggerapiresponses/web/DefaultApiResponses.java
+++ src/main/java/by/naxa/stackoverfow/swaggerapiresponses/web/DefaultApiResponses.java
@@ -3,12 +3,18 @@ package by.naxa.stackoverfow.swaggerapiresponses.web;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * A convenient meta-annotation for Swagger API responses.
+ *
+ * @see ApiResponse
+ * @see ApiResponses
  */
+@Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @ApiResponses({
         @ApiResponse(code = 200, message = "Success"),
